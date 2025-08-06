9ef657dcf5cfa08025c074f03f530eeff467a178
ed cuss <edcussmusic@gmail.com>
2025-08-06 22:01:03 +0100

    feat: basic read

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,24 @@
 use polars::prelude::*;
-use std::fs::File;
+use std::{fs::File, path::Path, process};
 
 fn main() {
-    let file = File::open("./test_data.parquet").unwrap();
-    let df = ParquetReader::new(file).finish().unwrap();
+    let df = match read_df("./breast_cancer_dataset.csv", FileFormat::Csv) {
+        Err(_) => process::exit(1),
+        Ok(df) => df,
+    };
+    dbg!(&df);
+}
 
-    dbg!(df);
+enum FileFormat {
+    Parquet,
+    Csv,
+}
+
+fn read_df<P: AsRef<Path>>(path: P, file_type: FileFormat) -> PolarsResult<DataFrame> {
+    let file = File::open(path)?;
+
+    match file_type {
+        FileFormat::Parquet => ParquetReader::new(file).finish(),
+        FileFormat::Csv => CsvReader::new(file).finish(),
+    }
 }
